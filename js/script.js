236345b34791b1d5c312e55c5391a8e460b5d5af
BorgesLuiz <106447017+BorgesLuiz@users.noreply.github.com>
2022-06-04 17:40:36 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function teste() {
+    alert("Olá, sou uma nova função");
+}
+
+function validarForm() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let mensagem = document.getElementById("mensagem").value;
+
+    alert(nome);
+    alert(email);
+    alert(telefone);
+    alert(mensagem);
+}
+
